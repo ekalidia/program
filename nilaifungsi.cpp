16b9c2634919cf476b2d76a6aca8f755a19abc2a
Eka Lidia Wati <45529774+ekalidia@users.noreply.github.com>
2018-12-04 16:50:34 +0700

    Add files via upload

---
+++ nilaifungsi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    int y;
+    cout<< " X = ";
+    cin>>x;
+    cout<< " Y = ";
+    cin>>y;
+    cout<< " X + Y = " <<x+y<<endl;
+    cout<< " X - Y = " <<x-y<<endl;
+    cout<< " X * Y = " <<x*y<<endl;
+    cout<< " X / Y = " <<x/y<<endl;
+cin.get();
+return 0;
+
+}
