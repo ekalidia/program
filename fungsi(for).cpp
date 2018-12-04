a183cbdb946197878a0fb19f2e4f13dc8a264142
Eka Lidia Wati <45529774+ekalidia@users.noreply.github.com>
2018-12-04 17:28:15 +0700

    Create fungsi(for).cpp

---
+++ fungsi(for).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    int c;
+    for(c=0;c<10;c++)
+    {
+        cout<< "APLIKASI PROGRAM C++"<<endl;
+    }
+    return 0;
+}
