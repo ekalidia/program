a46dc568a0b02633a63f03efadf3b5620d2819fd
Eka Lidia Wati <45529774+ekalidia@users.noreply.github.com>
2018-12-04 16:21:20 +0700

    Add files via upload

---
+++ constanta.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+  const float phi = 3.14;
+
+  const char nama[] = "antonius";
+  float luas;
+  int main()
+  {
+  	printf("%f\n",phi);
+  	luas = 7 * 7 * phi;
+  	printf("%f\n",luas);
+  	printf("%s\n",nama);
+  	getch();
+}
